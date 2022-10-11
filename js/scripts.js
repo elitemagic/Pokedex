4b8606222eaa292cafc3bddd52cb11ddeecd31da
elitemagic <toddmerlin@hotmail.com>
2022-10-11 20:41:41 +1100

    Updated formatting and added Pokemon to display on webpage with height

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-let pokemonList=[{name: "bulbasaur", height: "2", type: "grass";},{name: "ivysaur", height: "2", type: "grass";},{name: "venusaur", height: "6", type: "grass";},{name: "charmander", height: "2", type: "fire";},{name: "charmelon", height: "3", type: "fire";}{name: "Charizard", height: "5", type: "fire";}]
+let pokemonList=[
+    {
+        name: "Bulbasaur", height: "2", type: "grass"
+    },
+    {
+        name: "Ivysaur", height: "2", type: "grass"
+    },
+    {
+        name: "Venusaur", height: "6", type: "grass"
+    },
+    {
+        name: "Charmander", height: "2", type: "fire"
+    },
+    {
+        name: "Charmelon", height: "3", type: "fire"
+    },
+    {
+        name: "Charizard", height: "5", type: "fire"
+    }
+];
+  
+// Display a list of all of the Pokemon and their height, commenting 'Wow' with pokemon over 5 in height
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >5 ){
+      document.write("<p>" + pokemonList[i].name + " " + "(height:" + pokemonList[i].height + "); Wow, thats big!");
+    }
+    else {
+      document.write("<p>" + pokemonList[i].name + " " + "(height:" + pokemonList[i].height + ");");
+    }
+  }
