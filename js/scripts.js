f58762833140ccaea81c4eebf7cc6ac0d28bcf90
elitemagic <toddmerlin@hotmail.com>
2022-10-18 13:38:55 +1100

    Updated function within IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,47 @@
-let pokemonList=[
-    {
+let pokemonRepository = (function() {
+    let pokemonList = [
+      {
         name: "Bulbasaur", height: "2", type: "grass"
-    },
-    {
+      },
+      {
         name: "Ivysaur", height: "2", type: "grass"
-    },
-    {
+      },
+      {
         name: "Venusaur", height: "6", type: "grass"
-    },
-    {
+      },
+      {
         name: "Charmander", height: "2", type: "fire"
-    },
-    {
+      },
+      {
         name: "Charmelon", height: "3", type: "fire"
-    },
-    {
+      },
+      {
         name: "Charizard", height: "5", type: "fire"
+      }
+    ];
+    // Display a list of all of the Pokemon and their height
+    function myLoopFunction(poke) {
+      document.write('<p>' + poke.name + ' (height:' + poke.height + ');');
     }
-];
-
-
-
-// Display a list of all of the Pokemon and their height
-
-function myLoopFunction(poke) {
-    document.write('<p>' + poke.name + ' (height:' + poke.height + ');');
-  }
   
-  pokemonList.forEach(myLoopFunction)
+    function getAll() {
+      return pokemonList;
+    }
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function addListItem(pokemon1) {
+    }
+  
+    pokemonList.forEach(myLoopFunction)
+  
+    return {
+      getAll: getAll,
+      add: add
+    };
+  
+  })();
+  
   
